dfb496d0582c3fa3349cd9e56af5e318d9b6b37a
happywood007 <113480966+happywood007@users.noreply.github.com>
2022-11-05 12:42:06 +0900

    add divide func commit

--- LoopCalc.py
+++ LoopCalc.py
@@ -13,22 +13,25 @@ def multiply(x, y):
     return x * y
 
 #This function divide two numbers
-def divide (x,y):
-    return x / y
+def divide (x, y):
+    if(y == 0):
+        return 0
+    else:
+        return x / y
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
-print("4.Divide") 
+print("4.Divide")
 
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3): ")
+    choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3'):
+    if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -42,10 +45,15 @@ while True:
             print(num1, "*", num2, "=", multiply(num1, num2))
 
         elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+            num = divide(num1, num2)
+            # except Divided by zero
+            if not num:
+                print("Error! Divided by zero!")
+            else:
+                print(num1, "/", num2, "=", num)
 
-        # check if user wants another calculation
         # break the while loop if answer is no
+
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
             break
